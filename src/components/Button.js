2ca009adb29d9d3d081ca30a047efeb6f2ec8fc2
zlederma <zlederma@ucsd.edu>
2022-11-14 07:40:37 -0700

    Added comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,17 @@
 import './Button.css';
 
-//Props: handleClick, category, val
+/*
+props-
+ cat : (operator, operand, other) - category of button
+ val - string displayed in the innerHTML of the button
+ setButton - lifts state up to get the button data to Calculator
+*/
 export default function Button(props) {
     return (
         <button className='button__button'
             type='button'
-            onClick={event => props.setButton({ val: props.val })}
+            onClick={() => props.setButton
+                ({ val: props.val, cat: props.cat })}
         >
             {props.val}
         </button>
