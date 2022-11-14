b3b0125992383606b47440802f10249d1d31f52d
zlederma <zlederma@ucsd.edu>
2022-11-14 15:30:11 -0700

    Switched to useRef hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,16 +1,49 @@
 import './Calculator.css';
 import Screen from './Screen';
 import Buttons from './Buttons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 //Not sure if I like how this is being used
 
 export default function Calculator() {
     //equation is an array of charactars that show the current equation on the screen
     const [equation, setEquation] = useState([]);
-    const [result, setResult] = useState(0);
-    const [lhs, setLhs] = useState(0);
-    const [rhs, setRhs] = useState(0);
+    // const [result, setResult] = useState();
+
+    //store as strings so I can concatenate onto them
+    let lastOperator = useRef("x").current;
+    const result = useRef();
+    const lhs = useRef();
+    const rhs = useRef();
+
+    const calculateResult = () => {
+        let res = 0;
+        switch (lastOperator) {
+            case '^':
+                // return math.parseFloat(lhs) + parseFloat(rhs)
+                break;
+            case '÷':
+                res = parseFloat(lhs.current) / parseFloat(rhs.current);
+                break;
+            case 'x':
+                res = parseFloat(lhs.current) * parseFloat(rhs.current);
+                break;
+            case '-':
+                res = parseFloat(lhs.current) - parseFloat(rhs.current);
+                break;
+            case '+':
+                res = parseFloat(lhs.current) + parseFloat(rhs.current);
+                break;
+            default:
+                throw new Error('val not found')
+            //error handling
+        }
+        res = res.toFixed(6);
+        //parsing again to get rid of trailing 0's
+        res = parseFloat(res);
+        res = res.toString()
+        return res;
+    }
 
     const operator = (button) => {
         if (equation.length === 0) {
@@ -20,37 +53,45 @@ export default function Calculator() {
         const cat = button.cat;
         //TODO parentheses logic 
         if (equation[equation.length - 1].cat === 'operand') {
-            setEquation([...equation, button])
+            lastOperator = button.val;
+            setEquation([...equation, button]);
         }
 
         if (equation[equation.length - 1].cat === 'operator') {
             //using spead operator to copy the array, but make it so that react sees it as a new array
             const temp = [...equation];
             temp[temp.length - 1] = button;
+            lastOperator = button.val;
             setEquation(temp);
         }
-        // switch (val) {
-        //     case '^':
-        //         break;
-        //     case '÷':
-        //         break;
-        //     case 'x':
-        //         break;
-        //     case '-':
-        //         break;
-        //     case '+':
-        //         break;
-        //     case '-':
-        //         break;
-        //     default:
-        //         throw new Error('val not found')
-        //     //error handling
-        // }
     }
 
     const operand = (button) => {
+        //TODO add logic for when the last value in equation is an operand
         setEquation([...equation, button]);
+        const val = button.val;
+        if (lhs.current === undefined) {
+            console.log("val:" + val);
+            lhs.current = val;
+            console.log("lhs:" + lhs.current);
+            return;
+        }
+        if (rhs === undefined && equation[equation.length - 1].cat === 'operand') {
+            lhs.current = lhs.current + button.val;
+            console.log("lhs:" + lhs)
+            return;
+        }
+        if (equation[equation.length - 1].cat === 'operand') {
+            // setRhs(rhs + button.val);
+            rhs.current = rhs.current + button.val;
+            result.current = calculateResult();
+            console.log("result" + result)
+            return;
+        }
 
+        rhs.current = button.val;
+        result.current = calculateResult();
+        console.log("result:" + result);
     }
 
     const del = () => {
@@ -122,13 +163,13 @@ export default function Calculator() {
         }
     }
 
-    useEffect(() => {
+    // useEffect(() => {
 
-    }, [equation]);
+    // }, [equation]);
 
     return (
         <div className='calculator__container'>
-            <Screen equation={equation} result={result} />
+            <Screen equation={equation} result={result.current} />
             <Buttons handleClick={handleClick} />
         </div>
     )
