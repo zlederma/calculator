e995cc499b9c7ac659c63b363dbaf80cf3261590
zlederma <zlederma@ucsd.edu>
2022-11-14 08:29:49 -0700

    added function to display the charactars on the screen

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,8 +1,22 @@
 import './Screen.css';
 export default function Screen(props) {
+
+    const showEquation = () => {
+        const charactars = [];
+        for (let i = 0; i < props.equation.length; i++) {
+            charactars.push(
+                <div className='screen__charactar'>
+                    {props.equation[i].val}
+                </div>)
+        }
+        return charactars;
+    }
+
     return (
         <div className='screen__container'>
-            <div className='screen__equation'>{props.equation}</div>
+            <div className='screen__equation'>
+                {showEquation()}
+            </div>
         </div>
     )
 }
