74fbce01612bf200cb1a96b7067bc5c06146e6dc
zlederma <zlederma@ucsd.edu>
2022-11-14 08:30:36 -0700

    refactored to use handleClick

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,20 +3,28 @@ import Screen from './Screen';
 import Buttons from './Buttons';
 import { useState, useEffect } from 'react';
 
+//Not sure if I like how this is being used
+
 export default function Calculator() {
     //use useState to lift up state in order to figure out what button is pressed
     const [button, setButton] = useState({});
-    const [equation, setEquation] = useState("");
+    //equation is an array of charactars that show the current equation on the screen
+    const [equation, setEquation] = useState([]);
+
+    //this will be the main function that handles all the logic for when a user clicks a button
+    const handleClick = (button) => {
+        setButton(button)
+    }
 
     useEffect(() => {
         //switch this to an array so that I can change the color of the operators
-        setEquation(button.val)
+        setEquation([...equation, button])
     }, [button]);
 
     return (
         <div className='calculator__container'>
             <Screen equation={equation} />
-            <Buttons setButton={setButton} />
+            <Buttons handleClick={handleClick} />
         </div>
     )
 }
