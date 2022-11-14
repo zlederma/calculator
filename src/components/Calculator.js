34a437ea86134fafc7a93d0d0ad03f24fa4e713c
zlederma <zlederma@ucsd.edu>
2022-11-13 20:14:33 -0700

    added code to get text displayed on the screen

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,21 @@
 import './Calculator.css';
 import Screen from './Screen';
 import Buttons from './Buttons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Calculator() {
     //use useState to lift up state in order to figure out what button is pressed
     const [button, setButton] = useState({});
+    const [equation, setEquation] = useState("");
+
+    useEffect(() => {
+        //switch this to an array so that I can change the color of the operators
+        setEquation(button.val)
+    }, [button]);
 
     return (
         <div className='calculator__container'>
-            <Screen />
+            <Screen equation={equation} />
             <Buttons setButton={setButton} />
         </div>
     )
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,8 @@
 import './Screen.css';
-export default function Screen() {
+export default function Screen(props) {
     return (
         <div className='screen__container'>
+            <div className='screen__equation'>{props.equation}</div>
         </div>
     )
 }
