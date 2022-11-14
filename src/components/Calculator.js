a449c01da595b39cba6ea5593299c86eddc610f2
zlederma <zlederma@ucsd.edu>
2022-11-14 09:10:44 -0700

    Added basic setup for how I plan on controlling clicks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,10 +10,83 @@ export default function Calculator() {
     const [button, setButton] = useState({});
     //equation is an array of charactars that show the current equation on the screen
     const [equation, setEquation] = useState([]);
+    const [result, setResult] = useState();
+
+    const operator = (val) => {
+
+    }
+
+    const operand = (val) => {
+
+    }
+
+    const other = (val) => {
+        switch (val) {
+            case 'del':
+                del();
+                break;
+            case 'C':
+                clear();
+                break;
+            case '()':
+                parentheses();
+                break;
+            case '(-)':
+                negative();
+                break;
+            case '.':
+                decimal();
+                break;
+            case '=':
+                equals();
+            default:
+            //error handling
+        }
+
+    }
+
+    const parentheses = () => {
+
+    }
+
+    const equals = () => {
+
+    }
+
+    const clear = () => {
+
+    }
+
+    const negative = () => {
+
+    }
+
+    const decimal = () => {
+
+    }
+
+    const del = () => {
+
+    }
 
     //this will be the main function that handles all the logic for when a user clicks a button
     const handleClick = (button) => {
         setButton(button)
+        const category = button.cat;
+        const val = button.val;
+        switch (category) {
+            case 'operator':
+                operator(val);
+                break;
+            case 'operand':
+                operand(val);
+                break;
+            case 'other':
+                other(val);
+                break;
+            default:
+            //error handling
+        }
     }
 
     useEffect(() => {
