9af77326f66433b0b7ea11dbfd99ccbf3843d8ae
zlederma <zlederma@ucsd.edu>
2022-11-15 08:27:19 -0700

    Added function to render equation

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,8 +1,15 @@
 import './Screen.css';
 export default function Screen(props) {
+    const showEquation = () => {
+        const equation = [];
+        props.equation.forEach(term => equation.push(
+            <div className='screen__term'>{term.val}</div>));
+        return equation;
+    }
     return (
         <div className='screen__container'>
-            <div className='screen__equation'>{props.equation}</div>
+            <div className='screen__equation'>{showEquation()}</div>
+            <div className='screen__result'>{props.result}</div>
         </div>
     )
 }
