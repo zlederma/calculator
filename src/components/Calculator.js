d32da9b704ae6df571c254ce8ff670a54d30b23d
zlederma <zlederma@ucsd.edu>
2022-11-15 08:08:10 -0700

    Added blank functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,6 +7,42 @@ export default function Calculator() {
     //use useState to lift up state in order to figure out what button is pressed
     const [equation, setEquation] = useState("");
 
+    const operator = (button) => {
+
+    }
+
+    const operand = (button) => {
+
+    }
+
+    const other = (button) => {
+
+    }
+
+    const del = (button) => {
+
+    }
+
+    const clear = (button) => {
+
+    }
+
+    const parentheses = (button) => {
+
+    }
+
+    const negative = (button) => {
+
+    }
+
+    const decimal = (button) => {
+
+    }
+
+    const equals = (button) => {
+
+    }
+
     //this will be the main function that handles all the logic for when a user clicks a button
     //should include updating result and equation
     const handleClick = (button) => {
