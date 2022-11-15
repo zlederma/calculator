56a92a46bbee869ab7012f1dddb214fd215e23e1
zlederma <zlederma@ucsd.edu>
2022-11-15 08:27:39 -0700

    Did some more basic set up

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,45 @@
 import './Calculator.css';
 import Screen from './Screen';
 import Buttons from './Buttons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Calculator() {
     //use useState to lift up state in order to figure out what button is pressed
-    const [equation, setEquation] = useState("");
+    const [equation, setEquation] = useState([]);
+    const result = useRef("")
 
     const operator = (button) => {
-
+        setEquation([...equation, button])
     }
 
     const operand = (button) => {
-
+        setEquation([...equation, button])
     }
 
     const other = (button) => {
+        const val = button.val;
+        switch (val) {
+            case 'del':
+                del();
+                break;
+            case 'C':
+                clear();
+                break;
+            case '()':
+                parentheses();
+                break;
+            case '(-)':
+                negative();
+                break;
+            case '.':
+                decimal();
+                break;
+            case '=':
+                equals();
+                break;
+            default:
+                throw new Error('val not found')
+        }
 
     }
 
@@ -64,7 +88,7 @@ export default function Calculator() {
 
     return (
         <div className='calculator__container'>
-            <Screen equation={equation} />
+            <Screen equation={equation} result={result.current} />
             <Buttons handleClick={handleClick} />
         </div>
     )
