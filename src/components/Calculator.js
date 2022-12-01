dbe70e13cffe1b268bbd471ed1f6e249a71a8853
zlederma <zlederma@ucsd.edu>
2022-11-30 17:25:16 -0800

    added button objects as constants

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,24 +4,29 @@ import Buttons from './Buttons';
 import { useState, useRef } from 'react';
 
 export default function Calculator() {
-    //(-) will become n
-    //use useState to lift up state in order to figure out what button is pressed
     const [equation, setEquation] = useState([]);
+    const [result, setResult] = useState("");
     const currEquation = useRef([]) //For synchronous updates
     let prevTerm = equation.length > 0 ? equation[equation.length - 1] : {};
-    console.log(prevTerm)
-    const [result, setResult] = useState("");
+
     //use this to keep track of how many opening(l) and closing(r) parentheses there are
     const lParenthesesCount = useRef(0);
     const rParenthesesCount = useRef(0);
     const areParenthesesClosed = lParenthesesCount.current === rParenthesesCount.current ? true : false;
 
-    // TODO Typeof anything / 0 is infinity
+    //Button objects
+    const lParentheses = { name: "parentheses", val: "(" }
+    const rParentheses = { name: "parentheses", val: ")" }
+    const multiply = { name: "operator", val: "x" }
+    const neg = { name: "operand", val: "-" }
+    const zero = { name: "operand", val: "0." };
+
+    //TODO Typeof anything / 0 is infinity
     //add parentheses
     //Solve for the case where there is an operand surrounded by parentheses.
+    //Have to pass in equation because the equation in useState does not update synchronously
     const cleanEquation = (equation) => {
         let clean = [...equation];
-        const rParentheses = { name: "parentheses", val: ")" }
         const numParentheses = lParenthesesCount.current - rParenthesesCount.current;
         for (let i = 0; i < numParentheses; i++) {
             clean.push(rParentheses);
@@ -44,38 +49,28 @@ export default function Calculator() {
     //Source: https://www.geeksforgeeks.org/convert-infix-expression-to-postfix-expression/
     const infixToPostfix = (cleanEquation) => {
 
-        let stack = []; //For stack operations, we are using C++ built in stack
+        let stack = [];
         let result = [];
 
         for (let i = 0; i < cleanEquation.length; i++) {
             let val = cleanEquation[i].val !== undefined ? cleanEquation[i].val : "";
             let name = cleanEquation[i].name !== undefined ? cleanEquation[i].name : "";
 
-            // If the scanned character is
-            // an operand, add it to output string.
             if (name === "operand") {
                 result.push(cleanEquation[i]);
             }
 
-            // If the scanned character is an
-            // ‘(‘, push it to the stack.
             else if (val === '(') {
-                const lParentheses = { name: "parentheses", val: "(" }
                 stack.push(lParentheses);
             }
 
-            // If the scanned character is an ‘)’,
-            // pop and to output string from the stack
-            // until an ‘(‘ is encountered.
             else if (val === ')') {
                 while (stack.length > 0 && stack[stack.length - 1].val !== '(') {
-                    console.log(stack[stack.length - 1].val)
                     result.push(stack.pop());
                 }
                 stack.pop()
             }
 
-            //If an operator is scanned
             else if (name === "operator") {
                 while (stack.length !== 0 && precedence(val) <= precedence(stack[stack.length - 1].val)) {
                     result.push(stack.pop());
@@ -93,22 +88,16 @@ export default function Calculator() {
 
     const calculatePostfix = (postfixEquation) => {
 
-        //create a stack
         let stack = [];
 
-        // Scan all characters one by one
         for (let i = 0; i < postfixEquation.length; i++) {
             const name = postfixEquation[i].name
             const val = postfixEquation[i].val
 
-            // If the scanned character is an operand (number here),
-            // push it to the stack.
             if (name === "operand") {
                 stack.push(parseFloat(val));
             }
 
-            //  If the scanned character is an operator, pop two
-            // elements from stack apply the operator
             else {
                 let term1 = stack.pop();
                 let term2 = stack.pop();
@@ -173,8 +162,6 @@ export default function Calculator() {
         }
 
         else if (prevTerm.val === ')') {
-            const lParentheses = { name: "parentheses", val: "(" }
-            const multiply = { name: "operator", val: "x" }
             setEquation([...equation, multiply, lParentheses, button]);
             currEquation.current = [...equation, multiply, lParentheses, button];
             lParenthesesCount.current = lParenthesesCount.current + 1;
@@ -187,7 +174,7 @@ export default function Calculator() {
         if (equation.length > 1) {
             let clean = cleanEquation(currEquation.current);
             let postfix = infixToPostfix(clean);
-            setResult(calculatePostfix(postfix));
+            setResult(calculatePostfix(postfix).toString());
         }
     }
 
@@ -216,72 +203,59 @@ export default function Calculator() {
             button.val = "("
             setEquation([...equation, button]);
             lParenthesesCount.current = lParenthesesCount.current + 1;
-            return;
         }
-        if (prevTerm.name === 'operator' || prevTerm.val === '(') {
+        else if (prevTerm.name === 'operator' || prevTerm.val === '(') {
             button.val = "("
             setEquation([...equation, button]);
             lParenthesesCount.current = lParenthesesCount.current + 1;
-            return;
         }
-        if ((prevTerm.name === 'operand' || prevTerm.val === ")") && !areParenthesesClosed) {
+        else if ((prevTerm.name === 'operand' || prevTerm.val === ")") && !areParenthesesClosed) {
             button.val = ")"
             setEquation([...equation, button]);
             rParenthesesCount.current = rParenthesesCount.current + 1;
-            return;
         }
-        if ((prevTerm.val === ")" && areParenthesesClosed) || lParenthesesCount.current === 0) {
+        else if ((prevTerm.val === ")" && areParenthesesClosed) || lParenthesesCount.current === 0) {
             button.val = "(";
-            const multiply = { name: "operator", val: "x" };
             setEquation([...equation, multiply, button]);
             lParenthesesCount.current = lParenthesesCount.current + 1;
-            return;
         }
-        if ((prevTerm.name === "operand" && prevTerm.val.endsWith("-")) || prevTerm.val.endsWith(".")) {
+        else if ((prevTerm.name === "operand" && prevTerm.val.endsWith("-")) || prevTerm.val.endsWith(".")) {
             console.log('Not allowed');
-            return;
         }
 
-        console.log('A case we were not expecting');
+        else {
+            console.log('A case we were not expecting')
+        }
 
     }
 
     const negative = () => {
-        const lParentheses = { name: "parentheses", val: "(" }
-        const multiply = { name: "operator", val: "x" }
-        const neg = { name: "operand", val: "-" }
         if (equation.length === 0 || prevTerm.name === "operator") {
             setEquation([...equation, lParentheses, neg]);
             lParenthesesCount.current = lParenthesesCount.current + 1;
-            return;
         }
 
-        if (prevTerm.val === "(") {
+        else if (prevTerm.val === "(") {
             setEquation([...equation, neg]);
-            return;
         }
 
-        if (prevTerm.val === ")") {
+        else if (prevTerm.val === ")") {
             setEquation([...equation, multiply, lParentheses, neg]);
             lParenthesesCount.current = lParenthesesCount.current + 1;
-            return;
         }
 
-        if (prevTerm.name === "operand" && prevTerm.val.endsWith("-")) {
+        else if (prevTerm.name === "operand" && prevTerm.val.endsWith("-")) {
             const temp = equation.slice(0, equation.length - 2);
             setEquation(temp);
             lParenthesesCount.current = lParenthesesCount.current - 1;
-            return;
         }
 
-        if (prevTerm.name === "operand" || prevTerm.val.endsWith(".")) {
+        else if (prevTerm.name === "operand" || prevTerm.val.endsWith(".")) {
             console.log('Not allowed');
         }
     }
 
     const decimal = (button) => {
-        const zero = { name: "operand", val: "0." };
-        const multiply = { name: "operator", val: "x" }
         if (prevTerm.val.includes(".")) {
             console.log('Not allowed');
             return;
@@ -306,11 +280,15 @@ export default function Calculator() {
 
     const equals = () => {
         if (prevTerm.name === "operand" || prevTerm.val === ")") {
-            let postFix = infixToPostfix(equation);
+            let clean = cleanEquation(equation)
+            let postFix = infixToPostfix(clean);
             let calculation = calculatePostfix(postFix);
             let term = { name: "operand", val: calculation.toString() }
             setResult("");
             setEquation([term]);
+
+            lParenthesesCount.current = 0;
+            rParenthesesCount.current = 0
         }
         else {
             console.log('Not allowed');
